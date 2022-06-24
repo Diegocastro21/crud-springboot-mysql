8c76f786543159d4dec7555c601a286c6fc203e3
Diegocastro21 <Diegocamilocastrooliveros@gmail.com>
2022-06-24 18:36:51 -0500

    manejo de plantillas

--- src/main/java/co/com/gm/domain/Persona.java
+++ src/main/java/co/com/gm/domain/Persona.java
@@ -1,27 +1,31 @@
 package co.com.gm.domain;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.*;
 import java.io.Serializable;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "persona")
-public class Persona implements Serializable {
+public class Persona implements Serializable{
 
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idPersona;
+
+    @NotEmpty
     private String nombre;
+
+    @NotEmpty
     private String apellido;
+
+    @NotEmpty
+    @Email
     private String email;
+
     private String telefono;
-
-
-
 }

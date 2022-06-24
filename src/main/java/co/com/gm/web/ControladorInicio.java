023c83e1c34e0ab35bf17ca4ebcb384376f31540
Diegocastro21 <Diegocamilocastrooliveros@gmail.com>
2022-06-24 18:36:05 -0500

    manejo de plantillas

--- src/main/java/co/com/gm/web/ControladorInicio.java
+++ src/main/java/co/com/gm/web/ControladorInicio.java
@@ -7,7 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @Slf4j
@@ -30,6 +34,26 @@ public class ControladorInicio {
     }
 
 
-    
+    @PostMapping("/guardar")
+    public String guardar(@Valid Persona persona, Errors errores){
+        if(errores.hasErrors()){
+            return "modificar";
+        }
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
+
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model){
+        persona = personaService.encontrarPersona(persona);
+        model.addAttribute("persona", persona);
+        return "modificar";
+    }
+
+    @GetMapping("/eliminar/{idPersona}")
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
+        return "redirect:/";
+    }
 
 }
